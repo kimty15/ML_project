7c4dbfe721be3d45cb827a7cbd6c392e9b1bfe74
kimty15 <ty44976@gmail.com>
2023-05-23 20:54:11 +0700

    Model trainer

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import dill
 from src.exception import CustomExpection
-
+from sklearn.metrics import r2_score
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -14,3 +14,17 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomExpection(e,sys)
+    
+def evaluate_models(X_train,y_train,X_test,y_test, models):
+    try:
+        report = {}
+        for model_name, model in models.items():
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            r2 = r2_score(y_test, y_pred)
+            report[model_name] = r2
+
+        return report
+    except Exception as e:
+        raise CustomExpection(e,sys)
+        
