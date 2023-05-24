52281ba1602011380f9329dd0a0aa45cdcf92b58
kimty15 <ty44976@gmail.com>
2023-05-24 19:53:27 +0700

    Hyperparameter tunning

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 from src.exception import CustomExpection
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -15,11 +16,15 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomExpection(e,sys)
     
-def evaluate_models(X_train,y_train,X_test,y_test, models):
+def evaluate_models(X_train,y_train,X_test,y_test, models,params):
     try:
         report = {}
         for model_name, model in models.items():
-            model.fit(X_train, y_train)
+            param = params[model_name]
+            gs = GridSearchCV(model,param,cv=3)
+            gs.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
             y_pred = model.predict(X_test)
             r2 = r2_score(y_test, y_pred)
             report[model_name] = r2
